58a93dc3f50ec275bf8ebc05fe490f725aeb2d41
unknown <maladroit1@likelion.org>
2020-06-15 13:21:58 +0900

    initial update

---
+++ src/main/java/com/jeonghyeong/TStoryDataProducerApplication.java
@@ -0,0 +1,13 @@
+package com.jeonghyeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TStoryDataProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TStoryDataProducerApplication.class, args);
+	}
+
+}
