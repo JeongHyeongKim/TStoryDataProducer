29b242b2f7ce72eeaa9f2d78e1ae00932e6e0bca
unknown <maladroit1@likelion.org>
2020-06-16 18:01:48 +0900

    kafka config 수정 진행중

--- src/main/java/com/jeonghyeong/producer/service/DataProducerService.java
+++ src/main/java/com/jeonghyeong/producer/service/DataProducerService.java
@@ -1,5 +1,8 @@
 package com.jeonghyeong.producer.service;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +17,25 @@ public class DataProducerService {
 
 	public String produceData(JSONObject jsonObject) {
 
-		return null;
+
+
+	}
+
+	private Properties getProducerProperties() {
+
+		Properties configs = new Properties();
+        configs.put("bootstrap.servers", "");
+        configs.put("acks", "all");
+        configs.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        configs.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
 
 	}
 
 }
+
+/*
+ * kafkaResult = producer.KafkaMsg(kafkaServerIP, kafkaKeySerializer,
+ * kafkaValueSerializer, kafkaAck, kafkaMaxRequest, kafkaSendBuffer,
+ * kafkaBufferMemory, s3Result, kafkaTopic)
+ */
